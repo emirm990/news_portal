18c74de73e642517265dc7974b3f585b79ac2893
Emir Mekić <emirmekic123@gmail.com>
2022-02-23 22:31:18 +0100

    update

--- src/features/home/homeAPI.js
+++ src/features/home/homeAPI.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 // A mock function to mimic making an async request for data
 export function fetchNews() {
-    const key = "a167066467594f269e8ed0f8c53bf5c9";
+    const key = process.env.REACT_APP_API_KEY;
     const country = "us";
-    const url = "https://newsapi.org/v2/top-headlines?country=" + country + "&apiKey=" + key;
+    const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    axios.defaults.headers.common = {
+        "X-API-Key": key,
+    };
+    const url = "https://newsapi.org/v2/top-headlines?country=" + country;
     return axios.get(url)
             .then(res => {
                 return res.data.articles;
